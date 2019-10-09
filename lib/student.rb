aeccb5e77ad0221ce4c1ad570028925419e7b1f2
Marianne Pyon <mariannepyon@gmail.com>
2019-10-09 18:26:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,9 +33,9 @@ class Student
   DB[:conn].execute(sql)
 end
 
-def self.students_below_12th_grade
+def self.students_below_12th_grade(x)
   sql = "SELECT * FROM students WHERE grade < 12"
-  DB[:conn].execute(sql)
+  DB[:conn].execute(sql,x)
   end
 
   def self.first_X_students_in_grade_10(x)
