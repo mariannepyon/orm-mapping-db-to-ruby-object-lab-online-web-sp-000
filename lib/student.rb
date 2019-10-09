367331b20e862e615285756a5b228df709118b91
Marianne Pyon <mariannepyon@gmail.com>
2019-10-09 17:15:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
     sql = <<-SQL
-    # remember each row should be a new instance of the Student class	    SELECT *
+    SELECT *
     FROM students
     SQL
     DB[:conn].execute(sql).collect do |row|
