0ead8239654632fd31edfffb00c2633eacfe9459
Marianne Pyon <mariannepyon@gmail.com>
2019-10-09 18:28:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,21 +63,21 @@ def self.students_below_12th_grade
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-#
-#   def self.create_table
-#     sql = <<-SQL
-#     CREATE TABLE IF NOT EXISTS students (
-#       id INTEGER PRIMARY KEY,
-#       name TEXT,
-#       grade TEXT
-#     )
-#     SQL
-#
-#     DB[:conn].execute(sql)
-#   end
-#
-#   def self.drop_table
-#     sql = "DROP TABLE IF EXISTS students"
-#     DB[:conn].execute(sql)
-#   end
-# end
+  # 
+  # def self.create_table
+  #   sql = <<-SQL
+  #   CREATE TABLE IF NOT EXISTS students (
+  #     id INTEGER PRIMARY KEY,
+  #     name TEXT,
+  #     grade TEXT
+  #   )
+  #   SQL
+  # 
+  #   DB[:conn].execute(sql)
+  # end
+  # 
+  # def self.drop_table
+  #   sql = "DROP TABLE IF EXISTS students"
+  #   DB[:conn].execute(sql)
+  # end
+end
